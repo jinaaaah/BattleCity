4476f656917d432c10d2435e1a2dbebc52c23bf6
jina <qqwwdj@gmail.com>
2017-11-18 21:18:01 +0900

    setting

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,21 @@
-public class Program {
-    public static void main(String[] args) {
+import processing.core.PApplet;
 
+public class Program extends PApplet{
+    @Override
+    public void settings() {
+        size(500,500);
+    }
+
+    @Override
+    public void draw() {
+        background(0);
+    }
+
+    @Override
+    public void setup() {
+        background(0);
+    }
+    public static void main(String[] args) {
+        PApplet.main("Program");
     }
 }
