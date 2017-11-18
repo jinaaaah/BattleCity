5da88dab4de9b4ed38160766f2657b8c17af1e83
dahye <dahye96@gmail.com>
2017-11-19 01:18:21 +0900

    program 충돌 해결

--- src/Program.java
+++ src/Program.java
@@ -94,22 +94,18 @@ public class Program extends PApplet {
     public void keyPressed(KeyEvent event) {
         //key에 따른 유저 업데이트, 가는 상태
         if(key == CODED){
+            user.setMoved(true);
             if(keyCode == UP){
                 user.setDir(Constants.MOVE_UP);
-                System.out.println("test");
             }else if(keyCode == DOWN){
                 user.setDir(Constants.MOVE_DOWN);
             }else if(keyCode == LEFT){
                 user.setDir(Constants.MOVE_LEFT);
             }else if(keyCode == RIGHT){
                 user.setDir(Constants.MOVE_RIGHT);
-            }
-        }
+            }else if(key == 'd'){
 
-        if(key == ' '){
-            Bullet bullet = new Bullet(this);
-            bullet.setTank(user);
-            bullets.add(bullet);
+            }
         }
 
     }
@@ -117,8 +113,7 @@ public class Program extends PApplet {
     @Override
     public void keyReleased() {
         //멈춤상태
-
-        user.setDir(Constants.STOP);
+        user.setMoved(false);
 
     }
 
