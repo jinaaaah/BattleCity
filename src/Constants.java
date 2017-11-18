c5e10ef5fde7898f4abd6e73747a34b9a8061f40
dahye <dahye96@gmail.com>
2017-11-18 21:41:40 +0900

    ResourceManager 작성

---
+++ src/Constants.java
@@ -0,0 +1,3 @@
+public class Constants {
+    final int TANK = 0;
+}
--- src/ResourceManger.java
+++ src/ResourceManger.java
@@ -1,3 +1,29 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ResourceManger {
-    
+    private static PApplet p;
+    private static HashMap<Integer, ArrayList<PImage>> imageBox;
+
+    public static void init(PApplet applet) {
+        p = applet;
+        imageBox = new HashMap<>();
+    }
+
+    public static void cropImage(int name, String path, int width, int height, int countX, int countY) {
+        ArrayList<PImage> images = new ArrayList<>();
+        for(int i = 0 ; i < countY ; i++) {
+            for(int j = 0 ; j < countX ; j++) {
+                images.add(p.loadImage(path).get(j * width, i * height, width, height));
+            }
+        }
+        imageBox.put(name, images);
+    }
+
+    public static PImage getImage(int name, int index) {
+        return imageBox.get(name).get(index);
+    }
 }
