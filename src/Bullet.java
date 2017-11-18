75330349b451c7a515257a070784075223581736
jina <qqwwdj@gmail.com>
2017-11-18 22:07:09 +0900

    PApplet Main추가

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,7 +1,7 @@
 import processing.core.PApplet;
 
 public class Bullet extends RenderObject {
-    public Bullet(PApplet pApplet, ResourceManager resourceManger) {
-        super(pApplet, resourceManger);
+    public Bullet(PApplet pApplet) {
+        super(pApplet);
     }
 }
